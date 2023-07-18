12d59f1310b29f3b39dba01c0dd694291d7c33c5
Malik Ghulam Subbhani <gsmalik030@gmail.com>
2023-07-18 15:48:35 +0200

    Added form and handlesubmit function

--- src/components/File.jsx
+++ src/components/File.jsx
@@ -1,9 +1,29 @@
-import React from 'react'
+import React, { useContext } from 'react'
+import { AppContext } from '../App'
 
 const File = () => {
+    const {latestPost, setLatestPost} = useContext(AppContext)
+
+    const hanleSubmit = (e) => {
+        e.preventDefault()
+        const data = new FormData()
+        data.append("post[title]", e.target.title.value)
+        data.append("post[image]", e.target.image.files[0])
+        sendToServer(data)
+    }
+    const sendToServer = (data) => {}
   return (
     <div>
-      File
+      <form onSubmit={(e)=>hanleSubmit(e)}>
+        <label htmlFor="title">Titile</label>
+        <input type="text" name="title" id="title"   />
+        <br />
+        <label htmlFor="image">Image</label>
+        <input type="file" name="image" id="image"   />
+        <br />
+        <button type='submit'>Upload</button>
+        
+      </form>
     </div>
   )
 }
